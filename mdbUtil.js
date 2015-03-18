451c766f1b48d5b7bc95bcbfc8f7b41af0a0a65d
Pulkit C. <pulkit27@gmail.com>
2015-03-18 20:20:31 +0530

    Update mdbUtil.js

--- mdbUtil.js
+++ mdbUtil.js
@@ -1,17 +1,41 @@
-var MongoClient = require( 'mongodb' ).MongoClient;
+try {
+  var mongoose = require('mongoose');
+	} catch (_) {
+		// workaround for mongoose
+		var prequire = require('parent-require')
+		, mongoose = prequire('mongoose');
+				}
 
-var _db;
-
+//users  schema
+var Users = new mongoose.Schema({
+	name: String,
+	email: String,
+	mobile: String,
+	updated_at: { type: Date, default: Date.now }
+								});
+//errors  schema
+var Errors = new mongoose.Schema({
+	errmsg: String,
+	updated_at: { type: Date, default: Date.now },
+								});
 module.exports = {
 
-connectToServer: function( callback ) {
-MongoClient.connect( "mongodb://127.0.0.1:27017/users", function( err, db ) {
-_db = db;
-return callback( err );
-} );
-},
+	open: function( callback ) {
+	mongoose.connect( "mongodb://pc:pc123789@ds055680.mongolab.com:55680/ppmongoz", function( err ) {
 
-getDb: function() {
-return _db;
-}
-};
+		return callback( err );
+																									});
+								},
+
+getUsersSchema: function() {
+	return mongoose.model('Users', Users);
+							},
+getErrorsSchema: function(){
+	return mongoose.model('Errors', Users);
+							},
+close: function( callback ){
+	mongoose.disconnect(function( err ) {
+		return callback( err );
+										});
+							}
+				};
